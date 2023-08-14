8ac160b9c3b732bf5dddc054f3a2b2aab53a7dee
Michael K <mKomelin@list.ru>
2023-08-14 12:32:28 +0300

    Checking the presence of an element on the page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,24 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+
+# абстрактный класс с вспомогательными методами для работы с драйвером
 class BasePage:
-    def __init__(self, browser, url):    # конструктор с созданием переменных экземпляра класса
+    def __init__(self, browser, url):
+        # Конструктор с созданием переменных экземпляра класса
         self.browser = browser
         self.url = url
 
-    def open(self):    # открытие необходимой страницы по url, указанному при создании экземпляра
+    def open(self):
+        # Открытие необходимой страницы по url, указанному при создании экземпляра
         self.browser.get(self.url)
+
+    def is_element_present(self, timeout, *locator):
+        # Проверка наличия элемента на странице. Возвращает элемент по заданному локатору.
+        # Если элемент отсутствует или превышено время ожидания timeout - возвращается False
+        try:
+            out = WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located(locator))
+        except (NoSuchElementException, TimeoutException):
+            out = False
+        return out
--- pages/main_page.py
+++ pages/main_page.py
@@ -2,7 +2,13 @@ from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 
+# Класс описываемый конкретную страницу и наследуемый от BasePage
 class MainPage(BasePage):
+    def should_be_login_link(self):
+        # Проверка наличия ссылки
+        assert self.is_element_present(10, By.CSS_SELECTOR, '#login_link'), 'Login link is not presented'
+
     def go_to_login_page(self):
+        # Переход на страницу login
         login_link = self.browser.find_element(By.CSS_SELECTOR, '#login_link')    # ищем элемент
         login_link.click()    # кликаем по нему
