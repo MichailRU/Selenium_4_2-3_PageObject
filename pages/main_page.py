2c608fb4e3e95454d9fe320d4332895818a386b1
Michael K <mKomelin@list.ru>
2023-08-14 14:20:06 +0300

    Added description of selectors locators.py and checks on the Login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,14 @@
 from pages.base_page import BasePage
-from selenium.webdriver.common.by import By
+from pages.locators import MainPageLocators
 
 
 # Класс описываемый конкретную страницу и наследуемый от BasePage
 class MainPage(BasePage):
     def should_be_login_link(self):
         # Проверка наличия ссылки
-        assert self.is_element_present(10, By.CSS_SELECTOR, '#login_link'), 'Login link is not presented'
+        assert self.is_element_present(10, *MainPageLocators.LOGIN_LINK), 'Login link is not presented'
 
     def go_to_login_page(self):
         # Переход на страницу login
-        login_link = self.browser.find_element(By.CSS_SELECTOR, '#login_link')    # ищем элемент
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)    # ищем элемент
         login_link.click()    # кликаем по нему
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,13 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 link = 'http://selenium1py.pythonanywhere.com/'
+
+
+def test_guest_should_see_login_link(browser):
+    # Проверка, что есть ссылка, которая ведет на логин
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,8 +16,10 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()    # выполняем метод страницы — переходим на страницу логина
 
 
-def test_guest_should_see_login_link(browser):
-    # Проверка, что есть ссылка, которая ведет на логин
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+def test_can_go_to_login_page(browser):
+    page = MainPage(browser, link)      # инициализация PageObject, передаем экземпляр драйвера и url адрес
+    page.open()                         # открываем страницу
+    page.should_be_login_link()         # проверка существования линка логина
+    page.go_to_login_page()             # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()   # проверка url, форм логина и регистрации
